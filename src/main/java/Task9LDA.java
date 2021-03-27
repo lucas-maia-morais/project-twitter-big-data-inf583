d1eb01d95b05b55c490a641821b3c69b2f7006dc
Lucas Morais <lucas@Lucas.Morais>
2021-03-27 22:42:02 +0100

    Final LDA

--- src/main/java/Task9LDA.java
+++ src/main/java/Task9LDA.java
@@ -1,5 +1,4 @@
 import static org.apache.spark.sql.functions.col;
-import static org.apache.spark.sql.functions.explode;
 import static org.apache.spark.sql.functions.from_unixtime;
 import static org.apache.spark.sql.functions.lower;
 import static org.apache.spark.sql.functions.split;
@@ -11,33 +10,24 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.apache.log4j.Level;
 
-import static org.apache.spark.sql.functions.callUDF;
-
-
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.MapFunction;
-import org.apache.spark.ml.Pipeline;
 import org.apache.spark.ml.clustering.LDA;
 import org.apache.spark.ml.clustering.LDAModel;
 import org.apache.spark.ml.feature.CountVectorizer;
 import org.apache.spark.ml.feature.CountVectorizerModel;
-import org.apache.spark.ml.feature.StringIndexer;
 import org.apache.spark.sql.AnalysisException;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoder;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.DataTypes;
 
 import scala.collection.JavaConversions;
 import scala.collection.mutable.WrappedArray;
 
 import org.apache.spark.ml.feature.Tokenizer;
-import org.apache.spark.ml.feature.RegexTokenizer;
 import org.apache.spark.ml.feature.StopWordsRemover;
-import org.apache.spark.mllib.clustering.OnlineLDAOptimizer;
 import org.apache.spark.sql.Encoders;
 
 
@@ -45,23 +35,6 @@ import org.apache.spark.sql.Encoders;
 public class Task9LDA {
     static public SparkSession spark;
     static public String[] vocabArray;
-    
-//    private Pipeline createPipeline() {
-//    	Tokenizer tokenizer = new Tokenizer()
-//    		.setInputCol("text")
-//    		.setOutputCol("tokens");
-//    	CountVectorizer countVectorizer = new CountVectorizer()
-//    		.setInputCol("tokens")
-//    		.setOutputCol("features")
-//    		.setMinDF((Double)params.getOrDefault(params.getMinFF()))
-//    		.setVocabSize((Integer)params.getOrDefault(params.getNumFeatures()));  
-//    	StringIndexer tagIndexer = new StringIndexer()
-//    		.setInputCol("tag")
-//    		.setOutputCol("label");
-//    	
-//    	Pipeline pipeline = new Pipeline().setStages(new PipelineStage[]{tokenizer, countVectorizer, tagIndexer});
-//    	return pipeline;
-//    }
     
     public static String[] topicWords(Row row) {
     	
@@ -76,6 +49,7 @@ public class Task9LDA {
     }
     
     public static Dataset<Row> preprocess(String timestamp1, String timestamp2, String fileName, String fileStopWords) throws AnalysisException {
+    	int vocabSize = 1000;
 
 
         // read file with sample tweets
@@ -83,12 +57,6 @@ public class Task9LDA {
         Dataset <Row> stopwords = spark.read().json(fileStopWords);
         List<String> listStopWords = stopwords.as(Encoders.STRING()).collectAsList();
         String[] arrayStopWords = listStopWords.toArray(new String[0]);
-        //List<Row> stopwordsList = stopwords.collectAsList();
-        //String[] stopWordsArray = new String[stopwordsList.size()];
-        //for (int i = 0, in)
-        //WrappedArray<Integer> s = (WrappedArray<Integer>) value.get(value.fieldIndex("termIndices"));
-        //List<Integer> intList = new ArrayList<Integer>();
-	    //intList.addAll(JavaConversions.seqAsJavaList(s));
         tweets.show(false);
 
 
@@ -96,41 +64,31 @@ public class Task9LDA {
         		.select(col("id"), col("text"), col("date"))
         		.filter(col("date").gt(timestamp1))
                 .filter(col("date").lt(timestamp2));
+        
+        Dataset<Row> tweetsWords = texts.withColumn("words", split(lower(texts.col("text")), "[\\s\\n,;.:?!'’\"/\\\\]"));
 
         texts.show(false);
         
-        Tokenizer tokenizer = new Tokenizer().setInputCol("text").setOutputCol("words");
-        
-        RegexTokenizer regexTokenizer = new RegexTokenizer()
-        		.setInputCol("text")
-        		.setOutputCol("words")
-        		.setPattern("\\W");
-        
-//        spark.udf().register(
-//        	      "countTokens", (Seq<?> words) -> words.size(), DataTypes.IntegerType);
-        
-        Dataset<Row> tokenized = tokenizer.transform(texts);
-        tokenized.select("text", "words")
-        		//.withColumn("tokens", callUDF("countTokens", col("words")))
-        		.show(false);
-        
-        Dataset<Row> regexTokenized = regexTokenizer.transform(texts);
-        regexTokenized.select("text", "words")
-            //.withColumn("tokens", callUDF("countTokens", col("words")))
-            .show(false);
-        
+//        Tokenizer tokenizer = new Tokenizer().setInputCol("text").setOutputCol("words");
+//        
+//        Dataset<Row> tokenized = tokenizer.transform(texts);
+//        tokenized.select("text", "words")
+//        		//.withColumn("tokens", callUDF("countTokens", col("words")))
+//        		.show(false);
+//        
         StopWordsRemover remover = new StopWordsRemover()
         		.setInputCol("words")
         		.setOutputCol("wordsNotFiltered")
         		.setStopWords(arrayStopWords);
         
-        Dataset<Row> tokenizedFiltered = remover.transform(tokenized);
+        //Dataset<Row> tokenizedFiltered = remover.transform(tokenized);
+        Dataset<Row> tokenizedFiltered = remover.transform(tweetsWords);
         tokenizedFiltered.show(false);
         
         CountVectorizerModel cvModel = new CountVectorizer()
         		.setInputCol("wordsNotFiltered")
         		.setOutputCol("features")
-        		.setVocabSize(1000)
+        		.setVocabSize(vocabSize)
         		//.setMinDF(2)
         		.fit(tokenizedFiltered);
         
@@ -145,8 +103,6 @@ public class Task9LDA {
     public static void LDACluster(Dataset<Row> df) {
     	int numTopics = 10;
     	int maxIterations = 100;
-    	int vocabSize = 1000;
-    	
     	
     	//long corpusSize = df.count();
     	//double mbf = 2.0/ maxIterations + 1.0 / corpusSize;
@@ -208,7 +164,9 @@ public class Task9LDA {
    		    	return topicWords;
    		  }
    		}, encoder);
-   		wordsTopic.show(10);
+   		wordsTopic.show(true);
+   		JavaRDD<String> wordsTopicRDD = wordsTopic.javaRDD();
+   		wordsTopicRDD.saveAsTextFile("output/wordsTopics");
    		
 
     	// Shows the result.
@@ -228,8 +186,8 @@ public class Task9LDA {
 //        sqlContext.setConf("spark.sql.broadcastTimeout",  "36000");
         sqlContext.setConf("spark.executor.memory",  "6g");
         
-        Dataset<Row> datapreprocessed = preprocess("2020-02-02 11:00:00", "2020-02-02 11:00:30",
-                "data/English/NoFilterEnglish2020-02-02", "data/English/stop_words_english.txt");
+        Dataset<Row> datapreprocessed = preprocess("2020-02-03 10:00:00", "2020-02-03 11:00:00",
+                "data/French/2020-02-03", "data/French/stop_words_french.txt");
         LDACluster(datapreprocessed);
 	}
 
